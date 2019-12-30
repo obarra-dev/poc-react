c0ec8d01a3b9bc14e08ce36648c90e8d64cf8cae
obarra <barra_omar@hotmail.com>
2019-12-30 16:46:06 -0300

    update state of app and forecast

--- wheather-app/src/App.js
+++ wheather-app/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import LocationList from './components/LocationList';
 import ForecastExtended from './components/ForecastExtended';
 import './App.css';
@@ -9,26 +9,40 @@ const cities = [
   'Salta, ar'
 ];
 
-//TODO estudiar scopes
-const onSelectedLocation = city => {
-  console.log(`OnSelection: ${city}`);
-};
+
+
+
+class App extends Component{
+  constructor(){
+    super();
+    this.state = {city: 'empty'};
+  }
+
+  //TODO estudiar scopes
+  onSelectedLocation = city => {
+    console.log(`OnSelection: ${city}`);
+    this.setState({city});
+  };
 
 //TODO ver que paso con el tap react de react developer tools, y pasar del dom al react element
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <LocationList cities={cities} 
-          onSelectedLocation={onSelectedLocation}/>
-      </header>
-      <body>
-        <div>
-          <ForecastExtended city="HOLA MUNDO"/>
-        </div>
-      </body>
-    </div>
-  );
+//TODO ver la diferencia entre class App  y function App
+  render(){
+    const {city} = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <LocationList cities={cities} 
+            onSelectedLocation={this.onSelectedLocation}/>
+        </header>
+        <body>
+          <div>
+            <ForecastExtended city={city}/>
+          </div>
+        </body>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
