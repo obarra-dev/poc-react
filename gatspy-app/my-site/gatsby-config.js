c15c33ca38a50c828c0eb19aa0f35c61fa1b518d
obarra <barra_omar@hotmail.com>
2020-06-14 00:29:48 -0300

    show json with graphql

--- gatspy-app/my-site/gatsby-config.js
+++ gatspy-app/my-site/gatsby-config.js
@@ -8,5 +8,13 @@ module.exports = {
   /* Your site config here */
   plugins: [
     'gatsby-plugin-postcss',
+    'gatsby-transformer-json',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/data/`
+      }
+    }
   ],
 }
