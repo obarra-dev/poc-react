c48c46447e51a6303b9751899d5c7c8b82807c43
obarra <omarbarra@pop-os.localdomain>
2023-05-22 15:53:45 -0300

    todo first

--- todo-app/src/App.tsx
+++ todo-app/src/App.tsx
@@ -1,35 +1,90 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useRef } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+type FormElemEvent = React.FormEvent<HTMLFormElement>;
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface Task {
+  name: string;
+  done: boolean;
 }
 
-export default App
+function App(): JSX.Element {
+  const [newTask, setNewTask] = useState<string>("");
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const taskInput = useRef<HTMLInputElement>(null);
+
+  function handleSubmit(e: FormElemEvent): void  {
+    e.preventDefault();
+    addTask(newTask);
+    setNewTask("");
+    taskInput.current?.focus();
+  };
+
+  function addTask(name: string): void  {
+    const newTasks: Task[] = [...tasks, { name, done: false }];
+    setTasks(newTasks);
+  };
+
+  function toggleDoneTask  (i: number): void  {
+    const newTasks: Task[] = [...tasks];
+    newTasks[i].done = !newTasks[i].done;
+    setTasks(newTasks);
+  };
+
+  function removeTask (i: number): void {
+    const newTasks: Task[] = [...tasks];
+    newTasks.splice(i, 1);
+    setTasks(newTasks);
+  };
+
+  return (
+    <div className="container p-4">
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <div className="card">
+            <div className="card-header">
+              <h1 className="text-center">Tasks ReactTs</h1>
+            </div>
+            <div className="card-body">
+              <form onSubmit={handleSubmit}>
+                <input
+                  type="text"
+                  value={newTask}
+                  onChange={e => setNewTask(e.target.value)}
+                  ref={taskInput}
+                  className="form-control"
+                  required
+                  autoFocus
+                />
+                <button className="btn btn-success btn-block mt-2">Save</button>
+              </form>
+              {tasks.map((t: Task, i: number) => (
+                <div key={i} className="card card-body mt-2">
+                  <h2 style={{ textDecoration: t.done ? "line-through" : "" }}>
+                    {t.name}
+                  </h2>
+                  <div>
+                    <button
+                      onClick={() => toggleDoneTask(i)}
+                      className="btn btn-secondary"
+                    >
+                      {t.done ? "✓" : "✗"}
+                    </button>
+                    <button
+                      onClick={() => removeTask(i)}
+                      className="btn btn-danger"
+                    >
+                      🗑
+                    </button>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
