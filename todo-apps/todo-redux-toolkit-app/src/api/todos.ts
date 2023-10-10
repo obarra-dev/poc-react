129a301ac4f6f08f0f4aecb8c94ccb43a5a5d04b
obarra <omarbarra@pop-os.localdomain>
2023-10-10 01:13:52 -0300

    using json

--- todo-apps/todo-redux-toolkit-app/src/api/todos.ts
+++ todo-apps/todo-redux-toolkit-app/src/api/todos.ts
@@ -1,3 +1,6 @@
 import axios from "axios";
 
-export const fetchTodos = () => axios("http://localhost:3006/todos");
+// export const fetchTodos = () => axios("http://localhost:3006/todos");
+
+export const fetchTodos = () => axios("data/todos.json");
+
